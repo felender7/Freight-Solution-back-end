dc6605fcace1da6dc2b1b62c046689900add5378
felender7 <felender7@gmail.com>
2026-04-16 15:05:46 +0200

    Allow CORS for  production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200"
+    origins "http://localhost:4200", "http://localhost:3000", "https://freight-solution-front.web.app"
 
     resource "*",
       headers: :any,
