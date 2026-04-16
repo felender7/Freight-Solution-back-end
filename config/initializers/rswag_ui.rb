576f4c55f12b7511951c90a747041348a689f7ed
felender7 <felender7@gmail.com>
2026-04-16 11:12:32 +0200

    update config for production

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,5 +1,3 @@
 Rswag::Ui.configure do |c|
-  c.openapi_endpoint "/api-docs/v1/swagger.yaml", "API V1 Docs"
-  # Configure the route format for OpenAPI specs
-  c.api_listener_path = "/api-docs"
+  c.swagger_endpoint "/api-docs/v1/swagger.yaml", "API V1 Docs"
 end
