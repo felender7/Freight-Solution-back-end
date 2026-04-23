1c447304e4aef55ae80a026312a6e31fc416810f
felender7 <felender7@gmail.com>
2026-04-23 16:39:25 +0200

    Enhance User details with System Audit Log and standardized dashboard UI

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ class Admin::UsersController < Admin::BaseController
 
   def show
     @user = User.find(params[:id])
+    @activity_logs = @user.activity_logs.order(created_at: :desc).limit(50)
   end
 
   def new
